215d53b2098fc353c5cf6c10c64492061201f314
Clae <56447997+clayruh@users.noreply.github.com>
2023-08-29 23:52:22 -0400

    create classes

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt", favorite_toy="Any"):
+        self.name = name
+        self.breed = breed
+        self.favorite_toy = favorite_toy
+
+    def bark(self):
+        print("Woof!")
+
+    def showing_self(self):
+        return self
+    
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
